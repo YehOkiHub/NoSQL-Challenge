81e51093eb74a00d780266dc4785e153dd1d0e7b
YehOkiHub <jeff077872@hotmail.com>
2023-02-06 00:20:28 -0500

    trying to fix timestamp issue

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,13 @@
 const moment = require("moment")
 module.export = (date) =>{
- return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+ return moment(date).format('MMMM DD YYYY, h:mm:ss a')
 }
+
+// function dateFormat(timeStamp){
+//     return timeStamp = moment().format("MMM Do YY");  
+
+// } 
+
+let dateFormat = moment().format('MMMM DD YYYY, h:mm:ss a')
+
+module.export = dateFormat
